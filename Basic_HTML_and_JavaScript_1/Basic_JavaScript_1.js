0d61fe3edae9921b8533b44d190a837eef5a1c6f
wreckseal <42881537+wreckseal@users.noreply.github.com>
2025-12-29 13:10:51 -0700

    Separated the My_First_Function into Basic_JavaScript_2.js

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,7 +1,1 @@
 alert("Basic Javascript!");
-
-function My_First_Function() {
-	var myString = "Hello, world!";
-	var result = myString.fontcolor("green");
-	document.getElementById("Rex").innerHTML = result;
-}
---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+	var myString = "Hello, world!";
+	var result = myString.fontcolor("green");
+	document.getElementById("Rex").innerHTML = result;
+}
