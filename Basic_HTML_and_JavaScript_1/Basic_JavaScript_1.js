d3780166c9a3ce394cba1aa98c2d538e463f3a61
wreckseal <42881537+wreckseal@users.noreply.github.com>
2025-12-29 12:21:43 -0700

    Added Basic_HTML_and_JavaScript_1 folder and files

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Basic Javascript!");
+
+function My_First_Function() {
+	var myString = "Hello, world!";
+	var result = myString.fontcolor("green");
+	document.getElementById("Rex").innerHTML = result;
+}
